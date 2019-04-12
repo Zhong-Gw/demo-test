e07eefedb4cd5baecddf2b167cd203f5e0a087ef
Zhong <123@qq.com>
2019-04-12 16:24:06 +0800

    第一次代码提交

---
+++ demo01/src/com/lx/demo01/Demo01.java
@@ -0,0 +1,13 @@
+package com.lx.demo01;
+
+/**
+ *    测试使用 eclipse  提交代码到GitHub
+ * @author Administrator
+ *
+ */
+public class Demo01 {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
