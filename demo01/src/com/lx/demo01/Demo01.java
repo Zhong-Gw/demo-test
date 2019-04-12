028d2b247c49467531ca18a65a9438830ae827de
Zhong <123@qq.com>
2019-04-12 16:50:09 +0800

    修改bug 打印直角三角形

--- demo01/src/com/lx/demo01/Demo01.java
+++ demo01/src/com/lx/demo01/Demo01.java
@@ -1,7 +1,8 @@
 package com.lx.demo01;
 
 /**
- *    测试使用 eclipse  提交代码到GitHub
+ * 测试使用 eclipse 提交代码到GitHub
+ * 
  * @author Administrator
  *
  */
@@ -9,9 +10,16 @@ public class Demo01 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		
-		for (int i = 0; i < 5; i++) {
-			System.out.println(i);
+
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j < i; j++) {
+
+				System.out.println("*");
+
+			}
+			
+			System.out.println();
 		}
 	}
 }
